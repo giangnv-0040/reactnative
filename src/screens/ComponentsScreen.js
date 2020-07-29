88360e651222b80ff94b16b1d58fe38a350431e9
VanGiang <giangnv91@gmail.com>
2020-07-29 21:15:26 +0700

    Lesson2: Rule of JSX and JSX solution (#3)
    * Lesson 2: Rule of JSX
    
    * Lesson 2: JSX solution

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.text}>ComponentsScreen</Text>;
+  const name = 'GiangNV';
+  return (
+    <View>
+      <Text style={styles.text}>Getting started with React Native</Text>
+      <Text sytle={styles.subHeader}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 45
+  },
+  subHeader: {
+    fontSize: 20
   }
 });
 
