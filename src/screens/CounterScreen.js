469596e3f81194e4afa48793a1402d17ac3c0a71
giangnv91 <giangnv91@gmail.com>
2020-08-05 22:06:22 +0700

    Lesson 6 (part6): Exercise Reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    return { ...state, counter: state.counter + action.playload};
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+    console.log(state);
     return (
         <View>
             <Button 
                 title="Increase"
                 onPress={() => {
-                    setCounter(counter + 1);
+                    dispatch({ playload: 1 });
                 }}
             />
             <Button 
                 title="Decrease"
                 onPress={() => {
-                    setCounter(counter - 1);
+                    dispatch({ playload: -1 });
                 }}
             />
-            <Text>Current counter: {counter}</Text>
+            <Text>Current counter: {state.counter}</Text>
         </View>
     );
 };
